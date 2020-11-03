51e7ef6ee0815ce697f30c0fda73e21395779613
nick-johnson333 <nickmj1999@gmail.com>
2020-11-03 16:02:46 -0600

    added db connection

---
+++ database.py
@@ -0,0 +1,50 @@
+import psycopg2
+
+conn = None
+def open_db():
+    try:
+        global conn
+        conn = psycopg2.connect(
+            host="ec2-52-1-95-247.compute-1.amazonaws.com",
+            port="5432",
+            database="d89uja0jkmio1h",
+            user="ijexgsielqcyxr",
+            password="90a3789a8b5ceea5f70502e7905a17f4c8b61fe7b88d856d2823af2d6c80bebd"
+        )
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        exit(-1)
+
+def close_db():
+    global conn
+    if conn is not None:
+        conn.close()
+
+def is_db_open():
+    global conn
+    return conn is not None
+
+def build_dicts(cursor, rows: list):
+    colnames = [desc[0] for desc in cursor.description]
+    all_data = []
+    for row in rows:
+        i = 0
+        data = {}
+        for col in colnames:
+            data[col] = row[i]
+            i+=1
+        all_data.append(data)
+    return all_data
+
+def getAllFromUserSuggestionsTable():
+    global conn
+    open_db() # HAVE AT START OF ALL FUNCTIONS
+    cur = conn.cursor()
+
+    cur.execute('SELECT * from user_suggestions')
+
+    rows = cur.fetchall()
+    data = build_dicts(cur,rows)
+    cur.close()
+    close_db() # HAVE AT END OF ALL FUNCTIONS
+    return data
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import json
+import database
 
 app = Flask(__name__)
+GET = ['GET']
+POST = ['POST']
+PUT = ['PUT']
+DELETE = ['DELETE']
 
 @app.route('/')
 def hello():
     return 'Hello, World 2'
+
+@app.route('/user_suggestions', methods=GET)
+def get_user_messages():
+    data = {}
+    data['data']  = database.getAllFromUserSuggestionsTable()
+    return json.dumps(data)
+
+
